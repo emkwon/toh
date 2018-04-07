880b12d1754ae57d2a2e24f5b09130850c23e990
dong <>
2018-04-07 15:01:17 +0900

    3장. new 키워드로 객체 생성
    function으로 호출할때와 생성자 함수의 차이점
    생성자 함수에서 this의 의미

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,12 +7,19 @@ import {Hero} from '../hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
+/*  hero: Hero = {
     id: 1,
     name: 'Winstorm',
-  };
+  };*/
 
-  constructor() { }
+  hero = new Hero();
+
+  constructor() {
+    // hero.id = 1; // 사용불가 (위에 hero라는 변수는 HeroesComponent 의 hero가 멤버변수로 생성됨)
+    this.hero.id = 1;
+    this.hero.name = 'Winstorm';
+
+  }
 
   ngOnInit() {
   }
